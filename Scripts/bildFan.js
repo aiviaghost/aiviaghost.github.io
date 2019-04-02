8d77763458875e539ab72fdf3e41c39ea1a1d880
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2019-04-02 23:31:15 +0200

    small fixes
    pretty neat

--- Scripts/bildFan.js
+++ Scripts/bildFan.js
@@ -1,20 +1,20 @@
 
 $(document).ready(function(){
-    $('#sp1').slick({
-      infinite: true,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      arrows: false,
-      //slidesToScroll: 4
-    });
+  $('#sp1').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    //slidesToScroll: 4
+  });
 
-    $('.imgs').slick({
-      infinite: true,
-      autoplay: true,
-      autoplaySpeed: 4000,
-      arrows: true,
-      slidesToShow: 3,
-      draggable: false,
-      //slidesToScroll: 4
-    });
+  $('.imgs').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: true,
+    slidesToShow: 3,
+    dots: true,
+    //slidesToScroll: 4
+  });
 });
