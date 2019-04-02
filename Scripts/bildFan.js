1fb564a628b70783a2f2e5e9cd485d5640bada48
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2019-04-02 10:38:09 +0200

    big good1
    stor bra 2

---
+++ Scripts/bildFan.js
@@ -0,0 +1,12 @@
+
+/*$("document").ready(function(){
+
+    let pics = ["Images/spring.jpg", "Images/sunny.jpg", "Images/autumn.jpg", "Images/damnGood.jpg"];
+    let x = 0;
+
+    setInterval(function(){
+        $(".para1").css({"background": "url("+pics[x%4]+")", "background-position": "center", "background-repeat": "no-repeat", "background-size": "cover"});
+        x++;
+    }, 4000);
+
+})*/
