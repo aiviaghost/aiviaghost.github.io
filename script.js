558360bf8d6904ea03282972df4a298c7103ecdd
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2019-07-05 16:00:25 +0200

    testing testing

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('mouseup', isUp);
 
 document.addEventListener('mousemove', e => {
     let spWidth = sp.offsetWidth + e.movementX;
-    if(btnDown && (spWidth + 17 <= window.innerWidth) && (spWidth >= 0.2 * window.innerWidth)){
+    if(btnDown && (spWidth + 17 <= window.innerWidth) && (spWidth >= 10)){
         sp.style.width = spWidth + "px";
         slider.style.left = slider.offsetLeft + e.movementX + "px";
     }
