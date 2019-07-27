3e9316c1e1d8a3c410d1e0098467354dacf60549
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2019-07-27 21:30:33 +0200

    sometimes a good text color

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let header = "Aiviaghost";
 let x = 0;
 function typewriter(){
     if(x < header.length){
-        document.getElementById("wack").textContent += header.charAt(x);
+        document.getElementById("wack1").textContent += header.charAt(x);
         document.getElementById("wack2").textContent += header.charAt(x);
         x++;
         setTimeout(typewriter, 150);
@@ -56,22 +56,25 @@ document.addEventListener('mousemove', e => {
 * This code sets a randomized color as the primary color of the webpage
 * It creates a hexcode by picking 6 characters from a list of 16 characters
 * The string generated is assigned to the custom property "--spColor"
+* A complimentary color is picked for the backgrounds of the ".site" elements
+* The complimentary color is calculated by picking a number via the formula:
+* 15 - (char in "hexcode1") = (new char for "hexcode2")
 */
 let hex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e","f"];
-let hexcode = "#";
+let hexcode1 = "#";
 for(let x = 0; x < 6; x++){
     let rand = Math.floor(Math.random()*16);
-    hexcode += hex[rand];
+    hexcode1 += hex[rand];
 }
-document.documentElement.style.setProperty('--spColor', hexcode);
+document.documentElement.style.setProperty('--spColor', hexcode1);
 
-let hexcode1 = "#";
+let hexcode2 = "#";
 for(let j = 1; j < 7; j++){
-    hexcode1 += convert(15 - convert(hexcode.charAt(j)));
+    hexcode2 += convert(15 - convert(hexcode1.charAt(j)));
 }
 let sites = document.getElementsByClassName("site");
 for(let k = 0; k < sites.length; k++){
-    sites[k].style.backgroundColor = hexcode1;
+    sites[k].style.backgroundColor = hexcode2;
 }
 
 function convert(c){
@@ -103,4 +106,38 @@ function convert(c){
         default:
             return c;
     }
+}
+
+/*
+* This code sets color of the text on elements with the class "site"
+* This is done by calculating the contrast between the background color and the text color
+* If the contrast is 4:1 or greater then the text color is set to white otherwise it is set to black
+*/
+function brightness(hexcode){
+    let r = sRGB(hexcode.substr(1, 2)) / 255.0;
+    r = (r <= 0.03928) ? r/12.92 : Math.pow(((r + 0.055) / 1.055), 2.4);
+
+    let g = sRGB(hexcode.substr(3, 2)) / 255.0;
+    g = (g <= 0.03928) ? g/12.92 : Math.pow(((g + 0.055) / 1.055), 2.4);
+
+    let b = sRGB(hexcode.substr(5, 2)) / 255.0;
+    b = (b <= 0.03928) ? b/12.92 : Math.pow(((b + 0.055) / 1.055), 2.4);
+
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b;
+}
+
+function sRGB(hex){
+    return parseInt(hex, 16);
+}
+
+let lumA = brightness("#ffffff");
+let lumB = brightness(hexcode2);
+let color = ((Math.max(lumA, lumB) + 0.05) / (Math.min(lumA, lumB) + 0.05)) > 4 ? "white" : "black";
+
+console.log(brightness("#ffffff"));
+console.log(brightness(hexcode2));
+console.log((brightness("#ffffff") + 0.05) / (brightness(hexcode2) + 0.05));
+
+for(let k = 0; k < sites.length; k++){
+    sites[k].style.color = color;
 }
