f5c6c3477e0e7b9d9abff0d677109ceda70f4a66
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2019-08-11 01:43:05 +0200

    regex update mainly

--- calc/graph.js
+++ calc/graph.js
@@ -2,17 +2,17 @@ let graphInput = document.getElementById("graphInput");
 graphInput.value = "ex. x^2 + 3x + 1";
 
 //RegEx is bae
-let regex = RegExp('(?:[0-9-+*/^()x]|abs|e\^x|ln|log|a?(?:sin|cos|tan)h?)+');
+let regex = RegExp('[^0-9x+\\-*/^() ]');
 
 graphInput.addEventListener('focus', () => {
-    if(!regex.test(graphInput.value)){
-        graphInput.style.color = "#c48a73";
+    graphInput.style.color = "#c48a73";
+    if(regex.test(graphInput.value)){
         graphInput.value = "";
     }
 })
 
 graphInput.addEventListener('blur', () => {
-    if(graphInput.value == "" /*|| !regex.test(graphInput.value)*/){
+    if(graphInput.value == "" /*|| regex.test(graphInput.value)*/){
         graphInput.style.color = "#b4b4b4";
         graphInput.value = "ex. x^2 + 3x + 1";
     }
