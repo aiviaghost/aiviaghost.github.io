383a14c317f961084a2fb2793ad022cbbf784487
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2019-10-12 17:50:50 +0200

    pico writeups

---
+++ pico2019/pico2019.js
@@ -0,0 +1,22 @@
+let container = document.getElementById("container");
+let nav = document.getElementById("nav");
+let header1 = document.getElementById("pico2019");
+let darkBody = "#202124";
+let darkContainer = "#262729";
+
+["load", "resize"].forEach(e => {
+    window.addEventListener(e, () => {
+        //container.style.left = (window.innerWidth - container.offsetWidth)/2 + "px";
+        nav.style.left = container.offsetLeft + "px";
+        header1.style.lineHeight = nav.offsetHeight + "px";
+    })
+})
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 0){
+        nav.style.backgroundColor = "#353535";
+    }
+    else{
+        nav.style.backgroundColor = darkContainer;
+    }
+})
