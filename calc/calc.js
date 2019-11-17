ead6c441c809864dedc2684efab779de52c0eaff
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2019-11-17 01:34:05 +0100

    scroll test

--- calc/calc.js
+++ calc/calc.js
@@ -24,6 +24,30 @@ numbers.children[11].addEventListener("pointerdown", evaluateExpression)
 // enable the "clear" / "C" button
 operators.children[0].addEventListener("pointerdown", () => expression.value = "")
 
-function evaluateExpression(){
+function evaluateExpression() {
     // evaluate expression and display result
 }
+
+/*
+* enable horizontal scrolling but disable normal keyboard input 
+* https://stackoverflow.com/questions/8308096/how-to-make-html-text-field-readonly-but-also-scrollable
+*/
+var readOnlyInputs = document.querySelectorAll('input[readonly]');
+function readOnlyClickHandler() {
+    this.removeAttribute('readonly');
+}
+function readOnlyBlurHandler() {
+    this.setAttribute('readonly');
+}
+function readOnlyKeypressHandler(event) {
+    event.preventDefault();
+}
+document.addEventListener('load', function () {
+    for (var i = 0; i < readOnlyInputs.length; i++) {
+        readOnlyInputs[i].classList.add('readonly');
+        // Add the functions to the events
+        readOnlyInputs[i].addEventListener('click', readOnlyClickHandler);
+        readOnlyInputs[i].addEventListener('blur', readOnlyBlurHandler);
+        readOnlyInputs[i].addEventListener('keypress', readOnlyKeypressHandler);
+    }
+});
